6ab25489ebd25c54b8a181a03e98913011292ccf
Mariano Lambir <mlambir@gmail.com>
2022-12-09 14:09:24 -0300

    day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,19 +1,18 @@
-use std::arch::x86_64::_mm256_and_pd;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
-use std::ops::{Range, };
+use std::ops::RangeInclusive;
 
-fn to_range(s: &str) -> Range<usize>{
+fn to_range(s: &str) -> RangeInclusive<usize>{
     let splitted:Vec<_> = s.split("-").collect();
-    splitted[0].parse::<usize>().unwrap()..splitted[1].parse::<usize>().unwrap()
+    splitted[0].parse::<usize>().unwrap()..=splitted[1].parse::<usize>().unwrap()
 }
 
 fn filter_line(l: &str) -> bool {
     let splitted:Vec<_> = l.split(",").collect();
     let r1 = to_range(splitted[0]);
     let r2 = to_range(splitted[1]);
-    (r1.start <= r2.start && r1.end >= r2.end) || (r2.start <= r1.start && r2.end >= r1.end)
+    (r2.contains(&r1.start()) && r2.contains(&r1.end())) || (r1.contains(&r2.start()) && r1.contains(&r2.end()))
 }
 
 fn process(file_path: &str) -> usize {
@@ -22,7 +21,23 @@ fn process(file_path: &str) -> usize {
         .lines().filter(|l|filter_line(l.as_ref().unwrap())).count()
 }
 
+fn filter_line2(l: &str) -> bool {
+    let splitted:Vec<_> = l.split(",").collect();
+    let r1 = to_range(splitted[0]);
+    let r2 = to_range(splitted[1]);
+    r2.contains(&r1.start()) || r2.contains(&r1.end()) || r1.contains(&r2.start()) || r1.contains(&r2.end())
+}
+
+fn process2(file_path: &str) -> usize {
+    let file = File::open(file_path).unwrap();
+    io::BufReader::new(file)
+        .lines().filter(|l|filter_line2(l.as_ref().unwrap())).count()
+}
+
 fn main() {
     println!("{}", process("./day4/test_data.txt"));
     println!("{}", process("./day4/data.txt"));
+    
+    println!("{}", process2("./day4/test_data.txt"));
+    println!("{}", process2("./day4/data.txt"));
 }
