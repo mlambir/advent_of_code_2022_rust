cdecda239e54010be890e2148222b395552f034c
Mariano Lambir <mlambir@gmail.com>
2022-12-09 13:22:19 -0300

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -28,7 +28,26 @@ fn process(file_path: &str) -> usize {
         .sum()
 }
 
+fn process2(file_path: &str) -> usize {
+    let file = File::open(file_path).unwrap();
+    let mut total = 0;
+    let lines = io::BufReader::new(file).lines().map(|s|s.unwrap()).collect::<Vec<String>>();
+    let chunks = lines.chunks(3);
+    for sl in chunks{
+        let mut chunk_lines = sl.iter().map(|l|l.chars().collect::<HashSet<char>>());
+        let mut hs = chunk_lines.next().unwrap();
+        for other in chunk_lines{
+            hs = hs.intersection(&other).copied().collect();
+        }
+        total = total + hs.iter().map(|c| char_value(*c)).sum::<usize>()
+    }
+    total
+}
+
 fn main() {
     println!("{}", process("./day3/test_data.txt"));
     println!("{}", process("./day3/data.txt"));
+    
+    println!("{}", process2("./day3/test_data.txt"));
+    println!("{}", process2("./day3/data.txt"));
 }
