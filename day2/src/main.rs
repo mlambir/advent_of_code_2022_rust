4378763b9dbb055e24e2926d51f340dd19a2c6fc
Mariano Lambir <mlambir@gmail.com>
2022-12-09 00:49:10 -0300

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -26,7 +26,36 @@ fn process(file_path: &str) -> usize {
         .sum::<usize>()
 }
 
+fn points2(play: &str) -> usize{
+    match play {
+        "A X" => 0+3,
+        "A Y" => 3+1,
+        "A Z" => 6+2,
+        
+        "B X" => 0+1,
+        "B Y" => 3+2,
+        "B Z" => 6+3,
+        
+        "C X" => 0+2,
+        "C Y" => 3+3,
+        "C Z" => 6+1,
+        _ => 0
+    }
+}
+
+fn process2(file_path: &str) -> usize {
+    let contents =
+        fs::read_to_string(file_path).expect(format!("missing file {file_path}").as_str());
+    contents
+        .split("\n")
+        .map(|play| points2(play))
+        .sum::<usize>()
+}
+
 fn main() {
     println!("{}", process("./day2/test_data.txt"));
     println!("{}", process("./day2/data.txt"));
+    
+    println!("{}", process2("./day2/test_data.txt"));
+    println!("{}", process2("./day2/data.txt"));
 }
