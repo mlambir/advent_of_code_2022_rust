bb319e776add16f36bdcde4ad1c4068ee9dc714a
Mariano Lambir <mlambir@gmail.com>
2022-12-13 09:32:05 -0300

    fmt

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,20 +3,20 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 
-fn char_value(c: char) -> usize{
-    if c.is_ascii_uppercase(){
-        c as usize - 'A' as usize + 27      
+fn char_value(c: char) -> usize {
+    if c.is_ascii_uppercase() {
+        c as usize - 'A' as usize + 27
     } else if c.is_ascii_lowercase() {
-        c as usize - 'a' as usize + 1      
-    } else{
+        c as usize - 'a' as usize + 1
+    } else {
         0
     }
 }
 
 fn process_line(l: &str) -> usize {
-    let splitted =  l.split_at(l.len()/2);
-    let s1:HashSet<char> = splitted.0.chars().collect();
-    let s2:HashSet<char> = splitted.1.chars().collect();
+    let splitted = l.split_at(l.len() / 2);
+    let s1: HashSet<char> = splitted.0.chars().collect();
+    let s2: HashSet<char> = splitted.1.chars().collect();
     s1.intersection(&s2).map(|c| char_value(*c)).sum()
 }
 
@@ -31,12 +31,15 @@ fn process(file_path: &str) -> usize {
 fn process2(file_path: &str) -> usize {
     let file = File::open(file_path).unwrap();
     let mut total = 0;
-    let lines = io::BufReader::new(file).lines().map(|s|s.unwrap()).collect::<Vec<String>>();
+    let lines = io::BufReader::new(file)
+        .lines()
+        .map(|s| s.unwrap())
+        .collect::<Vec<String>>();
     let chunks = lines.chunks(3);
-    for sl in chunks{
-        let mut chunk_lines = sl.iter().map(|l|l.chars().collect::<HashSet<char>>());
+    for sl in chunks {
+        let mut chunk_lines = sl.iter().map(|l| l.chars().collect::<HashSet<char>>());
         let mut hs = chunk_lines.next().unwrap();
-        for other in chunk_lines{
+        for other in chunk_lines {
             hs = hs.intersection(&other).copied().collect();
         }
         total = total + hs.iter().map(|c| char_value(*c)).sum::<usize>()
@@ -47,7 +50,7 @@ fn process2(file_path: &str) -> usize {
 fn main() {
     println!("{}", process("./day3/test_data.txt"));
     println!("{}", process("./day3/data.txt"));
-    
+
     println!("{}", process2("./day3/test_data.txt"));
     println!("{}", process2("./day3/data.txt"));
 }
